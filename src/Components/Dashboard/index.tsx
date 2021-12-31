080df3538ae45e32af71b4a38b390cbf3ae9cbdd
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2021-12-30 22:08:45 -0300

    Dashboard de todos os componentes

---
+++ src/Components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from '../Header';
+
+import { Container, Content } from './styles';
+
+export const Dashboard = () => {
+    return (
+        <Container>
+            <Content>
+                <Header />
+            </Content>
+        </Container>
+    );
+}
---
+++ src/Components/Dashboard/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: flex-start;
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    margin-right: 6vw;
+    margin-left: 6vw;
+    position: relative;
+    
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+    justify-content: space-between;
+`;
