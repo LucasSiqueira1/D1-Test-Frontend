09300d8e1881c990007a9fb79c62aacdab1dddd4
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2021-12-30 22:10:20 -0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Header } from './Components/Header';
+//import { Header } from './Components/Header';
+import { Dashboard } from './Components/Dashboard';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
-      <Header />
+      <Dashboard />
+      <GlobalStyle />
     </>
 
   );
