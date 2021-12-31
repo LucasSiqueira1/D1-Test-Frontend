abbbee6b5a4e63e800beb9d5fa68392225251882
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2021-12-30 22:25:19 -0300

    Header Left

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,27 @@
 import Tooltip from "react-simple-tooltip"
 
+import { Container, HeaderLeft, User, AcmeLogo } from './styles';
+import AcmeImg from '../../assets/images/acme-logo.png';
+
 export const Header = () => {
     return (
-        <div>Header</div>
+        <Container>
+            <HeaderLeft>
+                <Tooltip
+                    content="Antônio⠀da⠀Silva"
+                    padding={10}
+                    radius={10}
+                    placement='right'
+                    color='#FFFF'
+                    background='#3E4157'
+                >
+                    <User>A</User>
+                </Tooltip>
+
+                <AcmeLogo>
+                    <img src={AcmeImg} alt="Logo Empresa Acme." />
+                </AcmeLogo>
+            </HeaderLeft>
+        </Container>
     )
 }
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,1 +1,54 @@
 import styled from 'styled-components';
+
+export const Container = styled.header`
+    width: 100%;
+    height: 110px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 8px;
+
+    background-color: var(--background);
+`;
+
+export const HeaderLeft = styled.div`
+    width: 50%;
+    height: 100%;
+    position: relative;
+
+    display: flex;
+    align-items: center;
+`;
+
+export const User = styled.div`
+    width: 30px;
+    height: 30px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: var(--blue);
+    color: var(--white-light);
+    border-radius: 50%;
+`;
+
+export const AcmeLogo = styled.div`
+    width: 120px;
+    height: 40px;
+    margin-left: 50px;
+    position: absolute;
+    padding: 5px 10px 3px 20px;
+    border: 1px solid var(--box-lighter);
+    border-radius: 5px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: var(--white-light);
+    img {
+        width: 100%;
+    }
+`;
